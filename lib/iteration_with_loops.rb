b0e1d8b9ff18614b7d5ee385ea2151f9495d4864
Arthur Wilton <arthur.m.wilton@gmail.com>
2020-09-07 15:48:54 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -2,7 +2,7 @@ def find_even_values(src)
   count = 0
   while count < src.length do
     inner_count = 0
-    while condition do
+    while inner_count < src.length[count] do
       
       inner_count += 1
     end
